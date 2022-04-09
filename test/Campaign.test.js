1549f41789c2fde47c2298d31374360c6c3c9560
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-09 22:07:31 +0900

    update: Campaign.test.js, marks caller as the campaign manager

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -19,17 +19,22 @@ beforeEach( async () => {
         .send({ from: accounts[0], gas: '1000000'});
 
     await factory.methods.createCampaign('100').send({
-        from: accounts[0],
+        from: accounts[0],  // manager
         gas: '1000000'
     });
 
     [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaignFactory.interface), campaignAddress);
+    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
 })
 
 describe('Campaign', () => {
-    it('factory and campaign is ok', async () => {
+    it('factory and campaign is ok', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    })
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(manager, accounts[0]);
     })
 })
